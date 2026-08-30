8dd3a732cf05907e9cba2f79cb40a1722529f620
Daruuu <thedaruny16@gmail.com>
2026-08-30 23:31:35 +0200

    [db] implement organitazion module

--- backend/app/modules/organizations/model.py
+++ backend/app/modules/organizations/model.py
@@ -1,2 +1,35 @@
 # MODEL — organizations
 # Entidad/es SQLAlchemy de este dominio.
+from datetime import datetime
+
+from sqlalchemy import DateTime, BigInteger, Identify, String, func
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.core.database import Base
+
+
+class Organization(Base):
+    __tablename__ = "organizations"
+
+    id: Mapped[int] = mapped_column(
+        BigInteger,
+        Identify(),
+        primary_key=True
+    )
+    name: Mapped[str] = mapped_column(
+        String(50),
+        nullable=False
+    )
+
+    create_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        nullable=False,
+    )
+
+    update_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        onupdate=func.now(),
+        nullable=False,
+    )
