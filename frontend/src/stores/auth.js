a4dd65f4caa802c64efb6e2f3f42e6f523630158
lylfergu <ly@air-de-lylou.home>
2026-08-31 14:53:56 +0200

    pinia auth store created

--- frontend/src/stores/auth.js
+++ frontend/src/stores/auth.js
@@ -1,1 +1,16 @@
 // STORE auth — sesión/usuario; login, register, logout; usado por guards.
+
+import { defineStore } from "pinia";
+
+export const useAuthStore = defineStore("auth", {
+    state:()=>({
+        user:
+        login:
+        role:
+        register: false,
+        logout: 
+        isAuthenticate:
+        token/session:
+    })
+
+})
